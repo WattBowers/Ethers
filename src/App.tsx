33b3c5f3e62c356d663c34ebd09b5bb98cfdbf3b
Jackson Watt-Bowers <Wattbowers@gmail.com>
2022-06-14 12:29:51 -0400

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import { ethers } from "ethers";
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  
+  const [eth, setEth] = useState('')
+  const [address, setAddress] = useState('')
+  
+  const INFURA_ID = 'c70d9d442c00407c9b4efbf74e4a4054'
+  const provider = new ethers.providers.JsonRpcProvider(`https://mainnet.infura.io/v3/${INFURA_ID}`)
+
+ const main = async () => {
+    const balance = await provider.getBalance(`${address}`)
+    console.log(ethers.utils.formatEther(balance))
+    setEth(ethers.utils.formatEther(balance))
+  }
+  
+  
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input onChange={event => setAddress(event.target.value)}></input>
+      <button onClick={() => main()}> click me </button>
+      <h1>{eth}</h1>
     </div>
   );
 }
